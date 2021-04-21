d797bd4439151c9464c0770796709fa1ba782c78
tony <tonyducvo000@gmail.com>
2021-04-21 14:51:04 -0700

    expanded on filtering and output, modularized input data

--- scraper_proj/web_word_count.py
+++ scraper_proj/web_word_count.py
@@ -1,56 +1,109 @@
 import time
+from data_source import urlpage
+import re
 import pandas as pd
+from common_words import rem_words
 from collections import Counter
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-urlpage = 'https://www.cnn.com'
-spc_chars = '-.,\n()[]+&' #special chars
-res = []
+def scrape(urlpage):
+        pd.set_option('display.max_rows', None)
+        spc_chars = "+?.:%',$[]&"
+        spc_char_list = ['.', ',', '\n', '(', ')', '[', ']', '+', '&', '\\', ':', '-', '•', '•', '•', '?', '™', '', '|', '👩', '©']
 
-opts = Options()
-opts.headless = True
+        res = []
 
-driver = webdriver.Firefox(options=opts)
-driver.get(urlpage)
-#time.sleep(60)
+        opts = Options()
+        opts.headless = True
 
+        driver = webdriver.Firefox(options=opts)
+        driver.get(urlpage)
+        #time.sleep(60)
 
-#scroll to bottom of page, then
-#grab source via selenium driver
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+        time.sleep(3)
 
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-time.sleep(3)
+        source = driver.page_source
+        driver.close()
 
-source = driver.page_source
-driver.close()
+        # f = open("scratch.txt", "w", encoding="utf-8")
+        # f.write(source)
+        # f.close()
 
-# f = open("scratch.txt", "w", encoding="utf-8")
-# f.write(source)
-# f.close()
+        soupSrc = BeautifulSoup(source, features="html.parser")
+        for script in soupSrc(["script","style"]):
+                script.decompose()
 
+        strips = list(soupSrc.stripped_strings)
+        strips = list([word for line in strips for word in line.split()])
 
-#remove <script> and <style> tags and make the list
-soupSrc = BeautifulSoup(source, features="html.parser")
-for script in soupSrc(["script","style"]):
-        script.decompose()
+        # st = open("strips.txt", "w", encoding="utf-8")
+        #
+        # st.write("+++++++++++++++++++++++++++++++\n")
+        # st.write(urlpage + "\n")
+        #
+        # for index in strips:
+        #         st.write(str(index)+"\n")
+        # st.close()
 
-strips = list(soupSrc.stripped_strings)
+        for char in spc_char_list:
+                if char in strips:
+                        strips.remove(char)
 
-#make translation table, mapping special characters to None then
-#translate special characters to blank
-trans_table = str.maketrans('', '', spc_chars)
-res = [s.translate(trans_table) for s in strips]
+        trans_table = str.maketrans('', '', spc_chars)
+        res = [s.translate(trans_table) for s in strips]
 
+        #remove numbers in list
+        #res = [x for x in res if not (x.isdigit())]
+        #print(res)
 
+        #remove numbers from res list
+        pat_num = "^(\d+)$"
+        num_filt = list(filter(lambda x: not re.match(pat_num, x), res))
 
+        #remove simple one word (i.e., 'a' or 'A')
+        #or two letter words (e.g., 'As', 'by', 'go', etc), these don't hold much significance
+        #keep two capitalized letters (US, UN, EU, etc) since these have meaning
+        pat_word = "^(\w{1})$|^([A-Z][a-z])$|^([a-z]{2})$"
+        full_filt = list(filter(lambda x: not re.match(pat_word, x), num_filt))
 
-#get count for the words, sort it by count, and put in dataframe
-count = Counter(res).most_common()
-countSorted = sorted(count, key=lambda x: x[1], reverse=True)
-df = pd.DataFrame.from_records(countSorted, columns=['Word', 'Count'], index=None)
+        #remove other common words
+        for index_rm in rem_words:
+                for index_f in full_filt:
+                        if re.match(index_rm, index_f):
+                                full_filt.remove(index_f)
 
-print(df)
-print('Closing program now!!!')
+        #Any item filtered from above becomes '' in list, remove ''
+        for index in full_filt:
+                if index == '' or index == '--' or index == '—':
+                        full_filt.remove(index)
 
+        print(full_filt)
+        #res = [index.lower() for index in res ]
+
+        with open("scratch.txt", "w", encoding="utf-8") as scratch:
+                for index in full_filt:
+                        scratch.write(str(index) + "\n")
+
+        scratch.close()
+
+        #get count for the words, sort it by count, and put in dataframe
+        count = Counter(full_filt).most_common()
+        countSorted = sorted(count, key=lambda x: x[1], reverse=True)
+
+        df = pd.DataFrame.from_records(countSorted, columns=['Word', 'Count'], index=None)
+
+        file = open("result.txt", "w", encoding="utf-8")
+        file.write(urlpage)
+        file.write("\n")
+        file.write(str(df))
+        file.write("\n\n++++++++++++++++++++++++++++++++\n\n")
+        file.close()
+
+        print(f'Building {urlpage} table...')
+
+scrape(urlpage)
+# for index in urllist:
+#         scrape(index)
---
+++ scraper_proj2/web_word_count3.py
@@ -0,0 +1,56 @@
+import time
+import pandas as pd
+from collections import Counter
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+urlpage = 'https://www.cnn.com'
+spc_chars = '-.,\n()[]+&' #special chars
+res = []
+
+opts = Options()
+opts.headless = True
+
+driver = webdriver.Firefox(options=opts)
+driver.get(urlpage)
+#time.sleep(60)
+
+
+#scroll to bottom of page, then
+#grab source via selenium driver
+
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+time.sleep(3)
+
+source = driver.page_source
+driver.close()
+
+# f = open("scratch.txt", "w", encoding="utf-8")
+# f.write(source)
+# f.close()
+
+
+#remove <script> and <style> tags and make the list
+soupSrc = BeautifulSoup(source, features="html.parser")
+for script in soupSrc(["script","style"]):
+        script.decompose()
+
+strips = list(soupSrc.stripped_strings)
+
+#make translation table, mapping special characters to None then
+#translate special characters to blank
+trans_table = str.maketrans('', '', spc_chars)
+res = [s.translate(trans_table) for s in strips]
+
+
+
+
+#get count for the words, sort it by count, and put in dataframe
+count = Counter(res).most_common()
+countSorted = sorted(count, key=lambda x: x[1], reverse=True)
+df = pd.DataFrame.from_records(countSorted, columns=['Word', 'Count'], index=None)
+
+print(df)
+print('Closing program now!!!')
+
