af136d63e1144e19b65c25bcbfd20baef363aee1
tony <tonyducvo000@gmail.com>
2021-04-21 14:52:17 -0700

    added data_source and common_words files

---
+++ scraper_proj/common_words.py
@@ -0,0 +1,20 @@
+rem_words = [
+    '^[Aa]re$',
+    '^and$',
+    '^for$',
+    '^from$',
+    '^got$',
+    '^[Hh]as$',
+    '^[Hh]ow$',
+    '^[Ii]ts$',
+    '^than$',
+    '^that$',
+    '^[Tt]he$',
+    '^then$',
+    '^this$',
+    '^[Tt]o[o]?$',  #get rid of To, to, Too, too
+    '^[Ww]hat$',
+    '^[Ww]ho$',
+    '^with$',
+    '^[Yy]ou[r]?$'  #remove You, you, Your, your
+]
---
+++ scraper_proj/data_source.py
@@ -0,0 +1,10 @@
+urlpage = 'https://www.cnn.com'
+urllist = ['https://www.cnn.com',
+        'https://www.foxnews.com',
+        'https://www.washingtonpost.com/',
+        'https://www.theepochtimes.com/',
+        'https://www.nytimes.com/',
+        'https://www.huffpost.com/',
+        'https://www.usatoday.com/'
+
+        ]
